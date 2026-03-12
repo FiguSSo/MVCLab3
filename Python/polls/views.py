e39628b31062cc268fe271a9c08fef2464e46672
DESKTOP-CQN9MLJ\figus <figuss03@wp.pl>
2026-03-12 18:38:41 +0100

    tutorial 3

--- Python/polls/views.py
+++ Python/polls/views.py
@@ -1,4 +1,17 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
 def index(request):
-    return HttpResponse("Witaj w ankietach! To jest strona główna polls.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+def results(request, question_id):
+    return HttpResponse(f"Patrzysz na wyniki pytania {question_id}.")
+
+def vote(request, question_id):
+    return HttpResponse(f"Głosujesz na pytanie {question_id}.")

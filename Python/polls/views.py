1ce6be6555d2ff888da5d694a9519d02b99f781d
DESKTOP-CQN9MLJ\figus <figuss03@wp.pl>
2026-03-12 19:07:35 +0100

    tut 5

--- Python/polls/views.py
+++ Python/polls/views.py
@@ -33,3 +33,20 @@ def vote(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/results.html", {"question": question})
+    
+    from django.utils import timezone
+
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.utils import timezone
+
+def index(request):
+    latest_question_list = Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+    
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id, pub_date__lte=timezone.now())
+    return render(request, "polls/detail.html", {"question": question})
